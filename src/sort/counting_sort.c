c6d8534c8a0918653c20b255211b0f7c456126f7
shuyingsun <sunshuying1010@gmail.com>
2018-05-06 18:10:28 +0800

    Quick sort and counting sort

---
+++ src/sort/counting_sort.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include "../../lib/array/static_array_lib.h"
+
+int get_max_val(int *nums, int numsSize) {
+    int max = nums[0];
+    int idx;
+
+    for (idx = 1; idx < numsSize; idx++) {
+        if (nums[idx] > max)
+            max = nums[idx];
+    }
+
+    return max;
+}
+
+int get_min_val(int *nums, int numsSize) {
+    int min = nums[0];
+    int idx;
+
+    for (idx = 1; idx < numsSize; idx++) {
+        if (nums[idx] < min)
+            min = nums[idx];
+    }
+
+    return min;
+}
+
+void counting_sort(int *nums, int *result, int range, int numsSize) {
+    int idx;
+    int *counting = (int *)malloc(sizeof(int) * range);
+
+    if (counting == NULL) {
+        printf("Malloc failure.\n");
+        return;
+    }
+
+    bzero(counting, sizeof(int) * range);
+    print_num_array(counting, range);
+    for (idx = 0; idx < numsSize; idx++)
+        counting[nums[idx]]++;
+    for (idx = 1; idx < range; idx++)
+        counting[idx] += counting[idx - 1];
+    for (idx = numsSize - 1; idx >= 0; idx--) {
+        result[counting[nums[idx]]] = nums[idx];
+        counting[nums[idx]]--;
+    }
+    free(counting);
+}
+
+int main(int argc, char *argv[]) {
+    int numsSize = 0;
+    int *nums = create_num_array(&numsSize);
+    int *result = NULL;
+    int max, min;
+
+    if (numsSize == 0)
+        return 0;
+    print_num_array(nums, numsSize);
+
+    result = (int *)malloc(sizeof(int) * numsSize);
+    if (result == NULL) {
+        printf("Malloc failure.\n");
+        return 0;
+    }
+
+    max = get_max_val(nums, numsSize);
+    min = get_min_val(nums, numsSize);
+    counting_sort(nums, result, (max - min + 1), numsSize);
+
+    printf("\nAfter sorting:");
+    print_num_array(result, numsSize);
+
+    free(nums);
+    free(result);
+    return 0;
+}
---
+++ src/sort/quick_sort.c
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../../lib/array/static_array_lib.h"
+
+static int count = 0;
+
+int partition_dec(int *nums, int start, int end) {
+    int i = start - 1, j = start;
+    int key = nums[end], tmp;
+    bool same_elements = 1;
+
+    count++;
+
+    // Assume start < end
+    while (j < end) {
+        if (nums[j] != key)
+            same_elements = 0;
+        if (nums[j] > key) {
+            i++;
+            tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+        j++;
+    }
+    i++;
+    nums[end] = nums[i];
+    nums[i] = key;
+
+    if (same_elements) 
+        return (start + end) / 2;
+    return i;
+}
+
+int randomized_partition_dec(int *nums, int start, int end) {
+    int i = start - 1, j = start, random = end;
+    int key, tmp;
+    bool same_elements = 1;
+
+    count++;
+    random = start + (int)((end - start) * rand() / RAND_MAX);
+    if (random < end) {
+        tmp = nums[random];
+        nums[random] = nums[end];
+        nums[end] = tmp;
+        printf("The %d time of partition is using %d as key.\n", count, nums[end]);
+    }
+    key = nums[end];
+
+    // Assume start < end
+    while (j < end) {
+        if (nums[j] != key)
+            same_elements = 0;
+        if (nums[j] > key) {
+            i++;
+            tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+        j++;
+    }
+    i++;
+    nums[end] = nums[i];
+    nums[i] = key;
+
+    if (same_elements) 
+        return (start + end) / 2;
+    return i;
+}
+
+void quick_sort_dec(int *nums, int start, int end) {
+    int mid;
+
+    if (nums == NULL || start >= end)
+        return;
+
+    printf("\nbefore partitioning:");
+    print_num_array(nums + start, end - start + 1);
+    mid = randomized_partition_dec(nums, start, end);
+    printf("\nreturned mid: %d", mid);
+    print_num_array(nums + start, end - start + 1);
+    quick_sort_dec(nums, start, mid - 1);
+    quick_sort_dec(nums, mid + 1, end);
+}
+
+int hoare_partition_dec(int *nums, int start, int end) {
+    int key = nums[start], tmp;
+    int i = start - 1, j = end + 1;
+
+    while (1) {
+        do {
+            j--;
+        } while (nums[j] < key);
+        do {
+            i++;
+        } while (nums[i] > key);
+        if (i >= j)
+            return j;
+        tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
+
+void hoare_quick_sort_dec(int *nums, int start, int end) {
+    int mid;
+
+    if (nums == NULL || start >= end)
+        return;
+
+    printf("\nbefore partitioning:");
+    print_num_array(nums + start, end - start + 1);
+    mid = hoare_partition_dec(nums, start, end);
+    printf("\nreturned mid: %d", mid);
+    print_num_array(nums + start, end - start + 1);
+    hoare_quick_sort_dec(nums, start, mid);
+    hoare_quick_sort_dec(nums, mid + 1, end);
+}
+
+/*
+ * Handling for a situation when more than one elements in nums are the same.
+ * @param nums	the number array for sorting
+ * @param start	the start index of number array
+ * @param end	the end index of number array
+ * @param mid1	nums[start ... mid1-1] is larger than nums[mid1]
+ * @param mid2	nums[mid1 ... mid2] equals to each other
+ * 		nums[mid2+1 ... end] is less than nums[mid1]/nums[mid2]
+ */
+void partition2_inc(int *nums, int start, int end, int *mid1, int *mid2) {
+    int i = start - 1, j = start;
+    int key = nums[end], tmp;
+    *mid1 = start - 1;
+    *mid2 = start - 1;
+
+    printf("start=%d, end=%d\n", start, end);
+    while (j < end) {
+        printf("mid1=%d, mid2=%d, i=%d, j=%d, nums[j]=%d, key=%d\n", *mid1, *mid2, i, j, nums[j], key);
+        if (nums[j] == key) {
+            (*mid2)++;
+            tmp = nums[j];
+            nums[j] = nums[*mid2];
+            nums[*mid2] = tmp;
+        } else if (nums[j] < key) {
+            i++;
+            (*mid1)++;
+            (*mid2)++;
+            tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+
+            if (*mid1 != *mid2) {
+                nums[j] = nums[*mid2];
+                nums[*mid2] = tmp;
+            }
+        }
+        j++;
+    }
+    (*mid2)++;
+    tmp = nums[end];
+    nums[end] = nums[*mid2];
+    nums[*mid2] = tmp;
+}
+
+void quick_sort2_inc(int *nums, int start, int end) {
+    int mid1, mid2;
+
+    if (start >= end)
+        return;
+    printf("\nbefore partitioning:");
+    print_num_array(nums + start, end - start + 1);
+
+    partition2_inc(nums, start, end, &mid1, &mid2);
+
+    printf("\nreturned mid1: %d, mid2: %d", mid1, mid2);
+    print_num_array(nums + start, end - start + 1);
+    quick_sort2_inc(nums, start, mid1);
+    quick_sort2_inc(nums, mid2 + 1, end);
+}
+
+int main(int argc, char *argv[]) {
+    int numsSize = 0;
+    int *nums = create_num_array(&numsSize);
+
+    if (numsSize == 0)
+        return 0;
+    print_num_array(nums, numsSize);
+
+    //quick_sort_dec(nums, 0, numsSize - 1);
+    //hoare_quick_sort_dec(nums, 0, numsSize - 1);
+    quick_sort2_inc(nums, 0, numsSize - 1);
+    printf("\nAfter decreasing quick_sort with %d calling partition():", count);
+    print_num_array(nums, numsSize);
+
+    free(nums);
+    return 0;
+}
